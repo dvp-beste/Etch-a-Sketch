47f014e5562f60f5af4bca6a1b72a47438832028
Beste <besteeris@gmail.com>
2022-07-08 14:13:34 +0300

    Change var's to const or let

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var squareContainer = document.querySelector('#squareContainer');
+const squareContainer = document.querySelector('#squareContainer');
 
-var initialNumberOfSquares = 16;
+let initialNumberOfSquares = 16;
 
 // Create initial squares of 16x16
 
@@ -8,7 +8,7 @@ createSquares(initialNumberOfSquares);
 
 function createSquares(numberOfSquares) {
     for (let i = 0; i < Math.pow(numberOfSquares, 2); i++) {
-        var newDiv = document.createElement('div');
+        const newDiv = document.createElement('div');
         newDiv.className = 'squares';
         newDiv.style.width = `${100/numberOfSquares}%`;
         squareContainer.appendChild(newDiv);
@@ -20,37 +20,35 @@ function createSquares(numberOfSquares) {
 // Leave a trail by changing the backgroundcolor of squares
 
 function leaveTrail(){
-    var squares = document.querySelectorAll('.squares');
+    const squares = document.querySelectorAll('.squares');
 
     squares.forEach(item => {
         item.addEventListener('mouseover', changeColor);
     })
-
-    function changeColor(e){
-        e.target.style.backgroundColor = 'black';
-    }
 }
 
-
+function changeColor(e){
+    e.target.style.backgroundColor = 'black';
+}
 
 // Add popup asking for number of squares
 
-var popupButton = document.getElementById('popupButton');
+const popupButton = document.getElementById('popupButton');
 
 popupButton.addEventListener('click', askNumberOfSquares);
 
 function askNumberOfSquares(e) {
-    var newNumberOfSquares = prompt("Please enter a number between 1 and 100");
+    let newNumberOfSquares = prompt("Please enter a number between 1 and 100");
+    console.log({newNumberOfSquares});
     if (newNumberOfSquares === null){
         return;
-    } else {
-        while (!(newNumberOfSquares >= 1 && newNumberOfSquares <= 100)) {
-        newNumberOfSquares = prompt("This is not a number between 1 and 100. Please enter a number between 1 and 100.");
-        }
-        while (squareContainer.firstChild) {
-        squareContainer.firstChild.remove();
-        }
-        createSquares(newNumberOfSquares);
+    } 
+    while (!(newNumberOfSquares >= 1 && newNumberOfSquares <= 100)) {
+    newNumberOfSquares = prompt("This is not a number between 1 and 100. Please enter a number between 1 and 100.");
     }
+    while (squareContainer.firstChild) {
+    squareContainer.firstChild.remove();
+    }
+    createSquares(newNumberOfSquares);
 }
 
