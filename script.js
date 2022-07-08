747a112dc0a96bdbab0bf67c6fe5e8fed4f0a7b0
Beste <besteeris@gmail.com>
2022-07-08 18:54:08 +0300

    Change trailcolor according to user's selection
    Add eventlistener for the buttons that change the trailcolor value. Rework changecolor function with the trailcolor value.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const squareContainer = document.querySelector('#squareContainer');
+const colorButtons = document.querySelectorAll('.colors');
 
 let initialNumberOfSquares = 16;
+let trailColor = 'black';
+const r = 255;
+
 
 // Create initial squares of 16x16
-
 createSquares(initialNumberOfSquares);
 
+
+// Change trailColor value
+colorButtons.forEach(item => {
+    item.addEventListener('click', (e) => {
+        trailColor = e.target.id;
+        if (trailColor === 'clear') {
+            squares.forEach(item => {
+                item.style.backgroundColor = 'white';
+            })
+        }
+    })
+})
+
+// Create Squares
 function createSquares(numberOfSquares) {
     for (let i = 0; i < Math.pow(numberOfSquares, 2); i++) {
         const newDiv = document.createElement('div');
@@ -13,22 +30,38 @@ function createSquares(numberOfSquares) {
         newDiv.style.width = `${100/numberOfSquares}%`;
         squareContainer.appendChild(newDiv);
     }
+    
     leaveTrail();
     
 }
 
-// Leave a trail by changing the backgroundcolor of squares
-
+// Leave trail by changing the backgroundcolor of squares
 function leaveTrail(){
-    const squares = document.querySelectorAll('.squares');
-
+    squares = document.querySelectorAll('.squares');
+    
     squares.forEach(item => {
         item.addEventListener('mouseover', changeColor);
     })
 }
 
+// Change color according to the value of the trailcolor
 function changeColor(e){
-    e.target.style.backgroundColor = 'black';
+    if (trailColor === 'black' || trailColor === 'white') {
+        e.target.style.backgroundColor = trailColor;
+    } else if (trailColor === 'rainbow') {
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+    } else {
+        const currentColor = e.target.style.backgroundColor;
+        const rgbArr = currentColor.substring(4, currentColor.length-1)
+                .replace(/ /g, '')
+                .split(',');
+
+        if (currentColor !== 'black' && rgbArr[0] === rgbArr [1] && rgbArr[0] === rgbArr[2] && rgbArr[1] === rgbArr[2]){
+            e.target.style.backgroundColor = `rgb(${rgbArr[0]-r/10}, ${rgbArr[0]-r/10}, ${rgbArr[0]-r/10})`
+        } else {
+            e.target.style.backgroundColor = `rgb(${r-r/10},${r-r/10},${r-r/10})`;
+        }
+    }
 }
 
 // Add popup asking for number of squares
@@ -39,7 +72,6 @@ popupButton.addEventListener('click', askNumberOfSquares);
 
 function askNumberOfSquares(e) {
     let newNumberOfSquares = prompt("Please enter a number between 1 and 100");
-    console.log({newNumberOfSquares});
     if (newNumberOfSquares === null){
         return;
     } 
