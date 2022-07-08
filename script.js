ab875b823bf9712a554770372b8fac1c0b0f7a47
Beste <besteeris@gmail.com>
2022-07-08 07:45:10 +0300

    Set up the "hover" effect
    Change the background color of each square when the mouse passes over it so that the mouse can leave a pixelated trail.
    
    Center the container and change the border color of the squares.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var container = document.querySelector('.container');
 
 var pixelSize = 100;
+
 // Create squares 
 
 for (let i = 0; i < Math.pow(pixelSize, 2); i++) {
@@ -9,4 +10,17 @@ for (let i = 0; i < Math.pow(pixelSize, 2); i++) {
     newDiv.style.width = `${100/pixelSize}%`;
 
     container.appendChild(newDiv);
+}
+
+// Leave a trail by changing the backgroundcolor of squares
+
+var squares = document.querySelectorAll('.squares');
+
+
+squares.forEach(item => {
+    item.addEventListener('mouseover', changeColor);
+})
+
+function changeColor(e){
+    e.target.style.backgroundColor = 'black';
 }
